13826dd999c42fdadbf14f717438c6cd2b2f1a08
Josep Seto <jsetojseto@gmail.com>
2021-04-25 16:25:26 +0200

    fix package.json

--- lib/observer/observable.d.ts
+++
@@ -1,7 +0,0 @@
-export declare type Callback<T> = (event: T) => void;
-export declare class Observable<T> {
-    subscribe(callback: Callback<T>): void;
-    unsubscribe(callback: Callback<T>): void;
-    notify(event?: T): void;
-    private subscribers;
-}
--- lib/types/utility-types.d.ts
+++
@@ -1,8 +0,0 @@
-export declare type ClassMethodNames<T> = {
-    [K in keyof T]: T[K] extends Function ? K : never;
-}[keyof T];
-export declare type ClassMethods<T> = Pick<T, ClassMethodNames<T>>;
-export declare type ClassPropNames<T> = {
-    [K in keyof T]: T[K] extends Function ? never : K;
-}[keyof T];
-export declare type ClassProps<T> = Pick<T, ClassPropNames<T>>;
