1ca80e576519121f37dc26ede362800bacb9ab5a
jseto <jsetojseto@gmail.com>
2022-10-16 13:47:01 +0700

    fix: getFunction to deal with plain and persistent types

--- src/cloud-functions/cloud-functions-mock.spec.ts
+++ src/cloud-functions/cloud-functions-mock.spec.ts
@@ -1,42 +1,92 @@
-import { Persistent, persistent, PersistentObject, registerPersistentClass } from '../persistent/persistent'
+import { PersistentObject } from '../persistent/persistent'
+import { TestUser } from '../store/mocks/test-user'
 import { CloudFunctions } from './cloud-functions'
 import { CloudFunctionsMock } from './cloud-functions-mock'
-
-@registerPersistentClass( 'Wrapper' )
-class ParamWrapper extends Persistent {
-	constructor( a?: string, b?: number ) {
-		super()
-		this._a = a
-		this._b = b
-	}
-	@persistent _a: string
-	@persistent _b: number
-}
 
 describe( 'Cloud functions', ()=>{
 
 	beforeEach(()=>{
 		CloudFunctions.useCloudFunctionsService( new CloudFunctionsMock({
 			testNoParam: ():Promise<string> => Promise.resolve( 'test' ),
-			testAutoResolve: ( data: PersistentObject<ParamWrapper> ): Promise<PersistentObject<ParamWrapper>> => {
+			testArbitraryParamAndReturn: ( data: any ): Promise<any> => Promise.resolve( data.length ),
+			testPersistentParamAndReturn: ( data: PersistentObject<TestUser> ): Promise<PersistentObject<TestUser>> => {
 				return Promise.resolve( JSON.parse( JSON.stringify( data ) ) )
-			}
+			},
+			testPersistentParamAndPlainReturn: ( data: PersistentObject<TestUser> ): Promise<number> => {
+				return Promise.resolve( data.age )
+			},
+			testPlainParamAndPersistentReturn: ( data: number ): Promise<PersistentObject<TestUser>> => {
+				const user = new TestUser()
+				user.age = data
+				return Promise.resolve( user )
+			},
+			testWithoutParam: (): Promise<string> => Promise.resolve( 'Hello from the other side' ),
+			testWithoutReturn: ( data: string ): Promise<void> => Promise.resolve()
 		}))
 
 	})
 
-	it( 'should execute test cloud function without params', async ()=>{
+	it( 'should execute cloud function without params', async ()=>{
 		const testNoParam = CloudFunctions.instance.getRawFunction<never, string>( 'testNoParam' )
 		const result = await testNoParam()
 		expect( result ).toEqual( 'test' )
 	})
 
-	it( 'should execute test cloud function with params', async ()=>{
-		const testAutoResolve = CloudFunctions.instance.getFunction<ParamWrapper, ParamWrapper>( 'testAutoResolve' )
+	it( 'should execute cloud function with params', async ()=>{
+		const testArbitraryParamAndReturn = CloudFunctions.instance.getFunction<string, number>( 'testArbitraryParamAndReturn' )
 
-		const paramWrapper = new ParamWrapper( 'test', 30 )
-		const result = await testAutoResolve( paramWrapper )
-		expect( result._a ).toEqual( 'test' )
-		expect( result._b ).toBe( 30 )
+		const result = await testArbitraryParamAndReturn( 'Hello' )
+		expect( result ).toEqual( 5 )
 	})
+
+	it( 'should execute cloud function without params', async ()=>{
+		const testWithoutReturn = CloudFunctions.instance.getFunction<string>( 'testWithoutReturn' )
+
+		expect( testWithoutReturn( '' ) ).resolves.toBeUndefined()
+	})
+
+	it( 'should execute cloud function with void return', async ()=>{
+		const testWithoutParam = CloudFunctions.instance.getFunction<string>( 'testWithoutParam' )
+
+		const result = await testWithoutParam()
+		expect( result ).toEqual( 'Hello from the other side' )
+	})
+
+	it( 'should execute cloud function with params and return as Persistent', async ()=>{
+		const testPersistentParamAndReturn = CloudFunctions.instance.getFunction<TestUser, TestUser>( 'testPersistentParamAndReturn' )
+
+		const user = new TestUser()
+		user.age = 35
+		user.name = { firstName: 'Test User', lastName: 'as a user' }
+
+		const result = await testPersistentParamAndReturn( user )
+		expect( result.age ).toBe( 35 )
+		expect( result.name ).toEqual({ firstName: 'Test User', lastName: 'as a user' })
+	})
+
+	it( 'should execute cloud function with params as Persistent and return as plain', async ()=>{
+		const testPersistentParamAndPlainReturn = CloudFunctions.instance.getFunction<TestUser, number>( 'testPersistentParamAndPlainReturn' )
+
+		const user = new TestUser()
+		user.age = 35
+
+		const result = await testPersistentParamAndPlainReturn( user )
+		expect( result ).toBe( 35 )
+	})
+
+	it( 'should execute cloud function with params as plain and return as Persistent', async ()=>{
+		const testPlainParamAndPersistentReturn = CloudFunctions.instance.getFunction<number, TestUser>( 'testPlainParamAndPersistentReturn' )
+
+		const result = await testPlainParamAndPersistentReturn( 35 )
+		expect( result.age ).toEqual( 35 )
+	})
+
+	// it( 'should execute cloud function with params and return as Persistent', async ()=>{
+	// 	const testPersistentParamAndReturn = CloudFunctions.instance.getFunction<ParamWrapper, ParamWrapper>( 'testPersistentParamAndReturn' )
+
+	// 	const paramWrapper = new ParamWrapper( 'test', 30 )
+	// 	const result = await testPersistentParamAndReturn( paramWrapper )
+	// 	expect( result._a ).toEqual( 'test' )
+	// 	expect( result._b ).toBe( 30 )
+	// })
 })
--- src/cloud-functions/cloud-functions.ts
+++ src/cloud-functions/cloud-functions.ts
@@ -28,7 +28,7 @@ export class CloudFunctions {
 		return CloudFunctions._cloudFunctionsService.retrieveFunction( cloudFunction )
 	}
 
-	getFunction<P extends Persistent | undefined = undefined, R extends Persistent | undefined = undefined>( cloudFunction: string ): CloudFunction<P,R> {
+	getFunctionForPersistent<P extends Persistent | undefined = undefined, R extends Persistent | undefined = undefined>( cloudFunction: string ): CloudFunction<P,R> {
 		const callFunction = CloudFunctions._cloudFunctionsService.callFunction
 
 		const func = CloudFunctions._cloudFunctionsService.retrieveFunction<P,R>( cloudFunction )
@@ -38,6 +38,30 @@ export class CloudFunctions {
 				return Persistent.createInstance( result ) as R
 			}
 		}
+	}
+
+	getFunction<P, R=void>( cloudFunction: string ): CloudFunction<P,R> {
+		const callFunction = CloudFunctions._cloudFunctionsService.callFunction
+		const func = this.getRawFunction<P, R>( cloudFunction )
+
+		return async ( param?: P ) => {
+			const result = await callFunction( func, this.processParam( param ) )
+			return this.processResult( result )
+		}
+	}
+
+	private processParam<P>( param: P ): P | PersistentObject<P & Persistent> {
+		if ( !param ) return undefined
+		if ( param instanceof Persistent ) return param.toObject()
+		return param
+	}
+
+	private processResult<R>( value: R | PersistentObject<R & Persistent> ): R {
+		if ( !value ) return undefined
+		if ( ( value as PersistentObject<R & Persistent> ).__className ) {
+			return Persistent.createInstance( value as PersistentObject<R & Persistent> ) as R
+		}
+		return value as R
 	}
 
 	private static _cloudFunctionsService: CloudFunctionsService
