2d23df5c9a71adea10d6fa8dfa5256c011415d4a
jseto <jsetojseto@gmail.com>
2023-03-12 21:57:42 +0700

    document store

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,19 +2,48 @@ import { DocumentReference, Persistent } from '../persistent/persistent';
 import { DataSource } from './data-source';
 import { Model } from './model';
 
+/**
+ * The store is the main entry point for the data access layer.
+ * It provides methods to retrieve models for collections and subcollections.
+ * It also provides methods to populate property references with actual data from the store.
+ * You need to register a data source before using the store.
+ * @example
+ * // Register a data source
+ * Store.useDataSource( new FirestoreDataSource( firebase.firestore() ) )
+ * // Retrieve a model for a collection
+ * const model = Store.getModel( 'User' )
+ * // Retrieve a model for a subcollection
+ * const model = Store.getModelForSubCollection( user, 'Posts' )
+ * // Populate property references
+ * const user = await Store.populate( user )
+ */
 export class Store {
 	private constructor(){}
 
 	static error = { shouldBeRegistered: 'You should register a data source before using the data Store.' }
 
+	/**
+	 * Registers a data source to be used by the store.
+	 * You need to register a data source before using the store.
+	 * @param dataSource the data source to be used by the store
+	 */
 	static useDataSource( dataSource: DataSource ) {
 		this._dataSource = dataSource
 	}
 
+	/**
+	 * The data source currently used by the store
+	 * @returns the data source
+	 */
 	static get dataSource() {
 		return Store._dataSource
 	}
 
+	/**
+	 * Retrieves a model for a collection
+	 * @param classId the class name or an instance of the document type stored in the collection
+	 * @returns the model for the collection
+	 */
 	static getModel< T extends Persistent>( classId: T | string ): Model<T> {
 		if ( !Store._dataSource ) throw new Error( this.error.shouldBeRegistered )
 		return new Model<T>( Store._dataSource, classId )		
@@ -63,6 +92,11 @@ export class Store {
 		}
 	}
 
+	/**
+	 * Checks if an instance is populated
+	 * @param instance the instance or array of instances to be checked
+	 * @returns true if the instance is populated
+	 */
 	static isPopulated< T extends Persistent>( instance: T | readonly T[] ): boolean {
 
 		if ( Array.isArray( instance ) ) {
