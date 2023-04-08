0b21e24430a5fb2ea808c7ef8f003b80079d7d75
jseto <jsetojseto@gmail.com>
2023-04-08 19:56:11 +0200

    utility types to be strictNullCheck compliant

--- src/types/utility-types.ts
+++ src/types/utility-types.ts
@@ -93,7 +93,7 @@ type Concat<T, U> = `${ string & T }${ string & U }`
 export type PropPath<T extends {}, AllowedTypes=any, MaxDepth extends number = 3, Prefix = ''> = MaxDepth extends number? {
 	[ P in keyof T ]: T[P] extends Function? never : T[P] extends AllowedTypes? T[P] extends Primitive | ArrayLike<any>
 		? Concat<Prefix, P>
-		: Concat<Prefix, P> | PropPath <T[P], AllowedTypes, Decr[MaxDepth], `${ Concat<Prefix, P> }.`> : never
+		: Concat<Prefix, P> | PropPath <T[P] & {}, AllowedTypes, Decr[MaxDepth], `${ Concat<Prefix, P> }.`> : never
 }[ keyof T ] & string: never
 
 export type PropPathType<T, Path, MaxDepth extends number = 2> = MaxDepth extends number
