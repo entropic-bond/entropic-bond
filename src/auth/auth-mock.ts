1536dd3930ed3b010ce542837d0f2c68f7340927
jseto <jsetojseto@gmail.com>
2023-05-25 18:36:04 +0700

    fix: typo in auth mock

--- src/auth/auth-mock.ts
+++ src/auth/auth-mock.ts
@@ -63,7 +63,7 @@ export class AuthMock extends AuthService {
 		else return Promise.reject({ code: 'userNotFound', message: 'Test auth error' })
 	}
 
-	resendEmailVerification( email: string ) {
+	resendVerificationEmail( email: string ) {
 		const fakeUserExists = Object.values( this._fakeRegisteredUsers ).find( 
 			user => user.email === email 
 		)
