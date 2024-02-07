9b79d7dbdf6747ed616d729151d4dc9f56127e00
jseto <jsetojseto@gmail.com>
2024-02-07 19:16:52 +0700

    feat: esm and cjs

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vitest/config'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   test: {
 		globals: true,
 		exclude: ['**/node_modules', '**/dist', '.idea', '.git', '.cache','**/lib', '**/out'],
 	},
+	build: {
+		lib: {
+			entry: resolve( __dirname, 'src/index.ts' ),
+			name: 'entropic-bond',
+			fileName: 'entropic-bond'
+		}
+	},
+	plugins: [
+		dts()
+	]
 })
