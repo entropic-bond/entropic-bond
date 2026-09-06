927c4fc38dca65a4d4309542863b6232501af4e2
jseto <jsetojseto@gmail.com>
2026-09-07 01:51:52 +0700

    npm update

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
 	},
 	build: {
 		lib: {
-			entry: resolve( __dirname, 'src/index.ts' ),
+			entry: resolve( import.meta.dirname, 'src/index.ts' ),
 			name: 'entropic-bond',
 			fileName: 'entropic-bond'
 		},
